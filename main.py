a2651198351d244d37477e322de81f095574607d
Miguel Viegas <miegas4@users.noreply.github.com>
2017-10-09 13:31:01 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,46 @@
+# Daniela Rodrigues nr: 84919
+# Miguel Viegas 	nr: 84747
+# Grupo 6
+
+import search
+import utils
+
+# TAI color 
+# sem cor = 0 
+# com cor > 0 
+
+def get_no_color():     
+	return 0 
+
+def no_color (c):     
+	return c==0 
+
+def color (c):     
+	return c > 0
+
+
+
+# TAI pos 
+# Tuplo (l, c) 
+
+def make_pos (l, c):     
+	return (l, c) 
+
+def pos_l (pos):     
+	return pos[0] 
+
+def pos_c (pos):     
+	return pos[1]
+
+
+
+
+# Board Methods
+
+def board_find_groups(board):
+	# TO DO
+	return "To be implemented"
+
+def board_remove_group(board, group):
+	# TO DO
+	return "To be implemented"
